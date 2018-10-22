e402297deac64565cdf33dfcd4a925b624e2e1bf
GlennFetty6 <glimma85@hotmail.com>
2018-10-22 16:46:59 +0200

    fix

--- GuldtandVue/src/App.vue
+++ GuldtandVue/src/App.vue
@@ -5,8 +5,10 @@
         <header>
           <titletext>
             GULDTAND
-              <router-link v-if="authenticated" to="/login" v-on:click.native="logout()" replace>Logout</router-link>
           </titletext>
+          <logout>
+            <router-link v-if="authenticated" to="/login" v-on:click.native="logout()" replace>Logout &nbsp;</router-link>
+          </logout>
         </header>
         <mainButtons>
           <button class="button" id="homeButton" v-on:click="switchPanel(1)">Home</button>
@@ -18,11 +20,10 @@
       </preLogin>
       <router-view @authenticated="setAuthenticated"></router-view>
       <footer>
-        <footerleft>&nbsp;&nbsp;&nbsp; Copyright Glenn-ENFORCEMENT</footerleft>
+        <footerleft>&nbsp;&nbsp;&nbsp; Copyright GangsterGrillz-ENFORCEMENT</footerleft>
         <footercenter>GULDTAND DENTAL</footercenter>
-        <footerright>Created by GlimmaDesign  &nbsp;&nbsp;&nbsp;  </footerright>
+        <footerright>Created by MyRepo  &nbsp;&nbsp;&nbsp;  </footerright>
       </footer>
-      <!--<router-view></router-view>-->
     </main>
   </div>
 </template>
@@ -50,9 +51,9 @@ export default {
       },
       logout() {
         this.authenticated = false;
+        document.getElementById("preLogin").style.visibility = "hidden";
       },
       switchPanel(nr) {
-        alert(nr);
         if (nr == 1) {
           this.$router.replace({ name: "Home" });
 
@@ -119,60 +120,16 @@ export default {
 </script>
 
 <style>
-  body {
-    margin: 0;
-  }
-
   #app {
-    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
   }
 
-  main {
-    text-align: center;
-    margin-top: 40px;
-  }
-
-  header {
-    margin: 0;
-    height: 56px;
-    padding: 0 16px 0 24px;
-    background-color: #35495E;
-    color: #ffffff;
-  }
-
-    header span {
-      display: block;
-      position: relative;
-      font-size: 20px;
-      line-height: 1;
-      letter-spacing: .02em;
-      font-weight: 400;
-      box-sizing: border-box;
-      padding-top: 16px;
-    }
-
-  footerleft {
-    float: left;
-    color: black;
-  }
-
-  footercenter {
-    text-align: center;
-    color: black;
-  }
-
-  footerright {
-    float: right;
-    color: black;
-  }
-
   titletext {
     font-size: 44px;
     background: url(https://images.pexels.com/photos/988874/pexels-photo-988874.jpeg?auto=compress&cs=tinysrgb&h=350) -80px -80px;
-    color: red;
     -webkit-text-fill-color: transparent;
     -webkit-background-clip: text;
     font-weight: bold;
@@ -183,50 +140,28 @@ export default {
     text-decoration: double;
   }
 
+  logout {
+    float: right;
+    font-size: 24px;
+    margin: 21px auto;
+    color: black;
+  }
+
   body {
     margin: 0;
     background-image: url(./assets/mainbg.jpg);
     background-size: 100%;
   }
 
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    color: #2c3e50;
-  }
-
   main {
     text-align: center;
-    margin-top: 0px;
   }
 
   header {
-    margin: 0;
     height: 70px;
-    padding: 0px 0px 0px 0px;
     background-color: #568aa3;
-    color: #ffffff;
-    /*background: linear-gradient(to bottom, rgba(208,228,247,1) 0%,rgba(115,177,231,1) 24%,rgba(10,119,213,1) 50%,rgba(83,159,225,1) 79%,rgba(135,188,234,1) 100%);*/
     box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
-  }
-
-  body {
-    background-color: #568aa3;
-  }
-
-  header span {
-    display: block;
-    position: relative;
-    font-size: 20px;
-    line-height: 1;
-    letter-spacing: .02em;
-    font-weight: 400;
-    box-sizing: border-box;
-    padding-top: 16px;
-  }
-
-  mainButtons {
+    background: radial-gradient(circle, rgba(131,197,228,1) 17%, rgba(86,138,163,1) 45%, rgba(57,98,117,1) 86%);
   }
 
   .button {
@@ -248,12 +183,12 @@ export default {
   }
 
   .button:hover {
-    box-shadow: 0 12px 16px 0 rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
+    -webkit-filter: drop-shadow(12px 12px 12px #222);
+    filter: drop-shadow(5px 5px 5px #222);
   }
 
   footer {
     position: fixed;
-    left: 0;
     bottom: 0;
     width: 100%;
     background-color: #568aa3;
@@ -263,4 +198,20 @@ export default {
     box-shadow: 12px 12px 16px 16px rgba(0,0,0,0.24), 0 17px 50px 0 rgba(0,0,0,0.19);
     line-height: 30px;
   }
+
+  footerleft {
+    float: left;
+    color: black;
+  }
+
+  footercenter {
+    text-align: center;
+    color: black;
+  }
+
+  footerright {
+    float: right;
+    color: black;
+  }
+
 </style>
