3c9ba1a1fc41f5f98aa293665a0e16fd298a8b3d
Donia Ahmad <donia.ahmad92@hotmail.com>
2018-10-31 20:52:28 +0100

    added vee-validate in the main.js to make homepage work

--- GuldtandVue/src/main.js
+++ GuldtandVue/src/main.js
@@ -4,8 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import axios from 'axios'
+import VeeValidate from 'vee-validate'
 
 window.axios=axios
+Vue.use(VeeValidate);
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
