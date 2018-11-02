7a0b748253bc5c9e53d8407f72643c89a274dda2
Donia Ahmad <donia.ahmad92@hotmail.com>
2018-11-02 23:41:15 +0100

    iimplemented new elements in the home page

--- GuldtandVue/src/components/Home.vue
+++ GuldtandVue/src/components/Home.vue
@@ -1,7 +1,10 @@
 <template>
-      <div class="holder">
-   <p>To do list</p>
-      <form @submit.prevent="addTask">
+
+  <body>
+    
+     <div class="holder">
+           <p>To do list</p>  
+           <form @submit.prevent="addTask">
       <input type="text" placeholder="Enter todays tasks.. " v-model="Todo" v-validate="'min:5'" name="Todo">
       
       <transition name="alert-in" enter-active-class="animated flipInX" leave-active-class="animated flipOutX">
@@ -15,20 +18,58 @@
               <i class="fa fa-minus-circle" v-on:click="remove(index)"></i>
             </li>
          </transition-group>
-       </ul>    
+       </ul> 
     </div>
- 
-</template>
+   <aside>
+<body>
 
+  <button class="dynamic-component-demo-tab-button dynamic-component-demo-active">News</button>
+     <button class="dynamic-component-demo-tab-button dynamic-component-demo-active">Gold</button>
+        <ul class="dynamic-component-demo-posts-sidebar">
+            </ul> 
+               <div class="dynamic-component-demo-post-container">
+                  <div class="dynamic-component-demo-post">
+                      <h3>Todays News</h3>
+                        <div>
+                     <h6>- Dont wait for the storm to pass</h6>
+                     <h6>- today we have 5 patients that whants goldteeth</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+        </aside>   
+   </body>  
+
+</template>
+<script>
+
+new Vue({
+  el: '#dynamic-component-keep-alive-demo',
+  data: {
+    currentTab: 'News',
+    tabs: ['News', 'Gold']
+  },
+  computed: {
+    currentTabComponent: function () {
+      return 'tab-' + this.currentTab.toLowerCase()
+    }
+  }
+})
+
+</script>
 <script>
 export default {
+  
   name: 'Home',
  data(){
     return{
       Todo:'',
-  Home:[ ]
+  Home:[ ],
   }
 },
+
 methods:{
   addTask() {
     this.$validator.validateAll().then((result) => {
@@ -43,21 +84,47 @@ methods:{
     },
     remove(id){
       this.Home.splice(id,1);
-    }
+        }
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 @import "https://cdn.jsdelivr.net/npm/animate.css@3.5.1";
 @import "https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"; 
+.dynamic-component-demo-tab-button {
+  padding: 6px 10px;
+  border-top-left-radius: 3px;
+  border-top-right-radius: 3px;
+  border: 1px solid #ccc;
+  cursor: pointer;
+  background: #f0f0f0;
+  margin-bottom: -1px;
+  margin-right: -1px;
+}
 
+aside {
+  padding: auto ;
+  margin-left:65%;  
+  margin-right:1%;
+    background:auto;
+ }
 
+h3{
+  text-align: center;
+  text-underline-position: below;
+}
+.alert{
+  font-size: 15px;
+}
 .holder {
     background: auto;
-    margin-right: 50%;
-    margin-top: 50px;
+    margin-right:40%;
+    margin-left:1%;
+    
+   
   }
   ul {
     margin: 0;
@@ -67,7 +134,7 @@ methods:{
   }     
   
   ul li {
-    padding: 20px;
+    padding: 10px;
     font-size: 15px;
     background-color:white;
     border-left: 5px solid #35495E;
@@ -77,6 +144,7 @@ methods:{
     
   }
   p {
+    font-size: 20px;
     text-align:center;
     padding: 30px 0;
     color: black;
